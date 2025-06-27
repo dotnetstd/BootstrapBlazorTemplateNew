8879bdf41b9cb0c103050f643af6f6b2281ccd70
phang <movieliang@msn.com>
2025-06-27 11:42:08 +0800

    First commit

---
+++ src/BootstrapBlazorApp.Server/Data/LayoutOptions.cs
@@ -0,0 +1,24 @@
+﻿using BootstrapBlazor.Components;
+
+namespace BootstrapBlazorApp.Server.Data
+{
+    public class LayoutOptions
+    {
+        public bool FixedHeader { get; set; } = true;
+        public bool ShowSide { get; set; } = true;
+        public bool FullSide { get; set; }
+        public bool ShowFooter { get; set; } = true;
+        public bool FixedFooter { get; set; } = true;
+        public bool UseTabSet { get; set; } = true;
+        public bool FixedTab { get; set; } = true;
+        public TabStyle TabStyle { get; set; } = TabStyle.Chrome;
+        public bool ShowTabInHeader { get; set; }
+        public bool TabShowCloseButton { get; set; } = true;
+        public bool ShowTabRightClickMenu { get; set; } = true;
+        public bool ShowTabExtendButtons { get; set; }
+        public bool TabShowToolbar { get; set; } = true;
+        public bool Accordion { get; set; } = true;
+        public string? Theme { get; set; } = "color6";//可以默认""
+        public int Pages { get; set; } = 20;
+    }
+}
---
+++ src/BootstrapBlazorApp.Server/Extensions/BrowserStorageExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace BootstrapBlazorApp.Server.Extensions;
+
+public static class BrowserStorageExtensions
+{
+
+    public static async Task<TValue?> GetValueAsync<TValue>(this ProtectedLocalStorage localStorage, string key)
+    {
+        TValue? ret = default;
+        try
+        {
+            var v = await localStorage.GetAsync<TValue>(key);
+            if (v.Success)
+            {
+                ret = v.Value;
+            }
+        }
+        catch { }
+        return ret;
+    }
+
+    public static async Task<TValue?> GetValueAsync<TValue>(this ProtectedSessionStorage sessionStorage, string key)
+    {
+        TValue? ret = default;
+        try
+        {
+            var v = await sessionStorage.GetAsync<TValue>(key);
+            if (v.Success)
+            {
+                ret = v.Value;
+            }
+        }
+        catch { }
+        return ret;
+    }
+}
